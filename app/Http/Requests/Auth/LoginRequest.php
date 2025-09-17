d50eb8bb06b1a072c2e1a3699b2db2a9b68e6df5
Nadeem Sheikh <nadeemulhaqsheikh786@gmail.com>
2025-09-17 23:51:24 +0530

    Update LoginRequest.php

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -9,13 +9,16 @@ class LoginRequest extends FormRequest
 {
     use SanitizesInput;
 
-    public function authorize(): bool { return true; }
+    public function authorize(): bool
+    {
+        return true;
+    }
 
     public function rules(): array
     {
         return [
-            'email' => ['required','email'],
-            'password' => ['required','string'],
+            'email'    => ['required', 'email'],
+            'password' => ['required', 'string'],
         ];
     }
 
